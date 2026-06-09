172ffe4894e90d5b924833985150ca3bf4105550
sajmon <szymon17099@gmail.com>
2026-06-09 18:00:05 +0200

    Qt data dir fix

--- home.nix
+++ home.nix
@@ -89,6 +89,10 @@
   xdg = {
     enable = true;
     userDirs.enable = true;
+    systemDirs.data = [
+      "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+      "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+    ];
   };
 
   gtk.enable = true;
